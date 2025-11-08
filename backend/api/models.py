928c0bfbfc2190f7dabd6a54b9d5d5c249792168
Jason Chi <jason.chi52@gmail.com>
2025-11-08 11:01:46 -0500

    course and chapter model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    owner = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+
+class Chapter(models.Model):
+    course = models.ForeignKey(Course, on_delete= models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    is_public = models.BooleanField(default=False)
+
+
